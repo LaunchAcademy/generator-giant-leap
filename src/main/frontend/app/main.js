9e5815f99e43243000ccecd93c7acf554cc13208
Dan Pickett <dan.pickett@launchacademy.com>
2020-04-27 14:42:44 -0400

    upgrade react

--- src/main/frontend/app/main.js
+++ src/main/frontend/app/main.js
@@ -1,15 +1,9 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 import ReactDom from "react-dom";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      pets: []
-    }
-  }
-
-  componentDidMount() {
+const App = (props) => {
+  const [pets, setPets] = useState([])
+  const loadPets = () => {
     fetch("/api/v1/pets").then((resp) => {
       if(resp.ok) {
         return resp
@@ -18,18 +12,18 @@ class App extends React.Component {
         throw new Error(resp.Error)
       }
     }).then(resp => {
-      return resp.json();
+      return resp.json()
     }).then(petsPayload => {
-      this.setState({pets: petsPayload.content})
+      setPets(petsPayload.content)
     }) 
   }
+  useEffect(setPets, pets)  
 
-  render() {
-    const petListItems = this.state.pets.map((pet) => {
-      return (<li><h2>{ pet.name }</h2><p>{ pet.species}</p></li>)
-    })
-    return (<ul>{petListItems}</ul>)
-  }
+
+  const petListItems = this.state.pets.map((pet) => {
+    return (<li><h2>{ pet.name }</h2><p>{ pet.species}</p></li>)
+  })
+  return (<ul>{petListItems}</ul>)
 }
 
 ReactDom.render(<App />,document.getElementById("app"))
